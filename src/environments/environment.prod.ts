11a9a129ad8a1aa84f17d66dae540e9df5848621
ACS-CAIRO\tabuhmead <tabuhmead@acs-me.com>
2018-01-11 17:48:57 +0200

    upload project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAqmtMqwRJAerBmv-Qmt0vCNd23IVs5l04',
+    authDomain: 'oshop-001.firebaseapp.com',
+    databaseURL: 'https://oshop-001.firebaseio.com',
+    projectId: 'oshop-001',
+    storageBucket: '',
+    messagingSenderId: '665947278602'
+  }
+};
