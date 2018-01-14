43f320e08046f1988489ada4a9af48e0f8f8dc02
Tamer Fouad Abuhmead <tmrfouad@gmail.com>
2018-01-14 08:30:37 +0200

    correct firbase to firebase

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-import * as firbase from 'firebase';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
